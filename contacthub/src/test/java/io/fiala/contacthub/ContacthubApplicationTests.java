4cfb8a31d8a309fcb3689e4a19f9ab7722b6b9b1
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-03-18 00:33:24 +0200

    inital commit

---
+++ contacthub/src/test/java/io/fiala/contacthub/ContacthubApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fiala.contacthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContacthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
