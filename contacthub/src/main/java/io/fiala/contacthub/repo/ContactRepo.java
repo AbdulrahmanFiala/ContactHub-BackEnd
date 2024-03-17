3f39de2053843749ef233070985c32ecd153db94
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-03-18 00:33:38 +0200

    Create ContactRepo.java

---
+++ contacthub/src/main/java/io/fiala/contacthub/repo/ContactRepo.java
@@ -0,0 +1,13 @@
+package io.fiala.contacthub.repo;
+
+import io.fiala.contacthub.domain.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ContactRepo extends JpaRepository<Contact, String> {
+
+    Optional<Contact> findById(String id);
+}
