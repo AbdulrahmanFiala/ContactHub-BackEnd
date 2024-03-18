7b93d30a5e44b2a8ea59fd001fb9c941a4ee1e2e
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-03-18 02:12:23 +0200

    Implement the deleteMapping controller

--- contacthub/src/main/java/io/fiala/contacthub/resource/ContactResource.java
+++ contacthub/src/main/java/io/fiala/contacthub/resource/ContactResource.java
@@ -5,14 +5,7 @@ import io.fiala.contacthub.service.ContactService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -57,5 +50,10 @@ public class ContactResource {
     @GetMapping(path = "/image/{filename}", produces = { IMAGE_PNG_VALUE, IMAGE_JPEG_VALUE })
     public byte[] getPhoto(@PathVariable("filename") String filename) throws IOException {
         return Files.readAllBytes(Paths.get(PHOTO_DIRECTORY + filename));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteContact(@PathVariable(value = "id") String id) {
+         contactService.deleteContact(id);
     }
 }
